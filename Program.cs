40f21db3fc8462c5c67efcd278c26ad92c041fa5
Radomir <114557710+RadomirSz@users.noreply.github.com>
2023-09-27 14:45:36 +0200

    27-09

--- Program.cs
+++ Program.cs
@@ -1,74 +1,27 @@
 ﻿
-/*
-7.Dwie wieżyce, jedna wysokości 30 stóp, druga 40 stóp, oddalone są od siebie o 50 stóp. 
-Pomiędzy nimi znajduje się wodotrysk, do którego zlatują dwa ptaki z wierzchołków obu wieżyc 
-i lecąc z jednakową prędkością przybywają w tym samym czasie. 
-Napisz program, który obliczy jaka jest dłuższa odległość pozioma wodotrysku jednej z wieżyc?
+/*string l = Console.ReadLine();
+int s = int.Parse(Console.ReadLine());
+int liczba = (int)l[0]-48;
+for (int i = 1; i < l.Length; i++)
+{
+    liczba = liczba *s + (l[i] - 48);
+}
+Console.WriteLine(liczba);*/
+
+//bin -> dec
+/*string b = Console.ReadLine();
+int liczba = (int)b[0]-48;
+for (int i = 1; i < b.Length; i++) liczba = liczba * 2 + (b[i] - 48);
+Console.WriteLine(liczba);
 */
+//dec -> bin
 
-/*int w1 = 30;
-int w2 = 40;
-int odl = 50;
-
-for (int i = 1; i <= odl; i++)
+int d = int.Parse(Console.ReadLine());
+string bin = "";
+while (d > 0)
 {
-    if (w1 * w1 + i * i == w2 * w2 + (odl - i) * (odl - i))
-    {
-        if (i > odl / 2) Console.WriteLine(i);
-        else Console.WriteLine(odl - i);
-    }
-    
-}*/
-
-
-
-/*1. Napisz program, który pomnoży dwie macierze losowych liczb. 
-Niech user podający wymiary macierzy zadba o odpowiednie wymiary,
-tak aby mnożenie było możliwe. Macierze wypełnij losowymi liczbami.*/
-using System.Diagnostics.CodeAnalysis;
-
-Random r = new Random();
-int a = int.Parse(Console.ReadLine());
-
-int[,] T1 = new int[a,a];
-int[,] T2 = new int[a,a];
-int[,] Tw = new int[a,a];
-
-for (int i = 0; i < a; i++)
-{
-    for (int j = 0; j < a; j++)
-    {
-        T1[i, j] = r.Next(0, 10);
-    }
+    if (d % 2 == 1) bin= "1" + bin;
+    else bin = "0" + bin;
+    d /= 2;
 }
-
-for (int i = 0; i < a; i++)
-{
-    for (int j = 0; j < a; j++)
-    {
-        T2[i, j] = r.Next(0, 10);
-    }
-}
-
-for (int i = 0; i < a; i++)
-{
-    for (int j = 0; j < a;j++)
-    {
-        int suma = 0;
-        byte l = 0;
-        for (int x = 0; x < a; x++)
-        {
-            suma += T1[x, l] * T2[l, x];
-            l++;
-        }
-        Tw[i,j] = suma;
-    }
-}
-foreach (var i in T1) 
-    Console.WriteLine(i);
-
-
-foreach (var i in T2) Console.WriteLine(i);
-foreach (var i in Tw) Console.WriteLine(i);
-
-//duj hupa 
+Console.WriteLine(bin);
