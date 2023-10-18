9299dbb2d7761729bdf063f19399ddc1dfc8e794
The Fisher <114557710+RadomirSz@users.noreply.github.com>
2023-10-18 15:06:53 +0200

    Add files via upload

---
+++ 18-10.cs
@@ -0,0 +1,23 @@
+﻿
+const string login="";
+const string haslo = "";
+
+string Plogin = Console.ReadLine();
+string Phaslo = Console.ReadLine();
+
+if (haslo == Phaslo && login == Plogin)
+{
+   
+}
+
+
+
+
+bool trzezwosc(int wynik)
+{ 
+    Random r = new Random();
+    int a = r.Next(0, 20);
+    int b = r.Next(0, 20);
+    int c = r.Next(0, 20);
+    int[] T = {'-','+','/','*' };
+}
