c340241defd2fe46f6edb384f467831651ba8b04
RadomirSz <r.szpruta@gmail.com>
2023-10-29 23:54:10 +0100

    nowaying

--- logoalkolator/logoalkolator.cs
+++ logoalkolator/logoalkolator.cs
@@ -1,16 +1,21 @@
-﻿// const string login="";
-// const string haslo = "";
+﻿using System;
+using System.Data;
 
-// string Plogin = Console.ReadLine();
-// string Phaslo = Console.ReadLine();
+const string login="login";
+const string haslo = "haslo";
 
-// if (haslo == Phaslo && login == Plogin)
-// {
-//     float x = float.Parse(Console.ReadLine());
-//     if(trzezwosc());
-//     {}
-// }
+Console.WriteLine("Podaj login:");
+string Plogin = Console.ReadLine();
+Console.WriteLine("Podaj haslo:");
+string Phaslo = Console.ReadLine();
 
+if (haslo == Phaslo && login == Plogin)
+{
+       if(trzezwosc());
+       {
+       kalkulator();
+       }
+}
 
 
 bool trzezwosc()
@@ -19,61 +24,90 @@ bool trzezwosc()
     bool flaga = false;
     Random r = new Random();
 
-    do{
-    int a = r.Next(1, 10);
-    int b = r.Next(1, 10);
-    int c = r.Next(1, 10);
+        do{
+        int a = r.Next(1, 10);
+        int b = r.Next(1, 10);
+        int c = r.Next(1, 10);
 
-    int los = r.Next(1,5);
-    char x;
-    switch(los)
-        {
-            case 1:
-                x = '+';
-                break;
-            case 2:
-                x = '-';
-                break;
-            case 3:
-                x = '*';
-                break;
-            default:
-                x = '/';
-                break;
-        }
+        int los = r.Next(1,5);
+        char x;
+        switch(los)
+            {
+                case 1:
+                    x = '+';
+                    break;
+                case 2:
+                    x = '-';
+                    break;
+                case 3:
+                    x = '*';
+                    break;
+                default:
+                    x = '/';
+                    break;
+            }
 
-    int los2 = r.Next(1,5);
-    char y;
-    switch(los2)
-        {
-            case 1:
-                y = '+';
-                break;
-            case 2:
-                y = '-';
-                break;
-            case 3:
-                y = '*';
-                break;
-            default:
-                y = '/';
-                break;
-        }
+        int los2 = r.Next(1,5);
+        char y;
+        switch(los2)
+            {
+                case 1:
+                    y = '+';
+                    break;
+                case 2:
+                    y = '-';
+                    break;
+                case 3:
+                    y = '*';
+                    break;
+                default:
+                    y = '/';
+                    break;
+            }
     
-    System.Console.WriteLine($"Podaj wynik tego działania: {a}{x}{b}{y}{c}");
-    float wynik = 123;
-    float Pwynik = float.Parse(Console.ReadLine());
-    if(wynik == Pwynik)
-        {
-            flaga = true;
-            break;
-        }
-    proby++;
+        System.Console.WriteLine($"Podaj wynik tego działania: {a}{x}{b}{y}{c}");
+        string dzialanie = a.ToString();
+        dzialanie += x.ToString();
+        dzialanie += b.ToString();
+        dzialanie += y.ToString();
+        dzialanie += c.ToString();
+        Console.WriteLine(dzialanie);
+        DataTable dt = new();
+        var v = dt.Compute(dzialanie, "");
+        double wynik = Convert.ToDouble(v);
+        double Pwynik = double.Parse(Console.ReadLine());
+        if(wynik == Pwynik)
+            {
+                flaga = true;
+                break;
+            }
+        proby++;
     }
-    while(proby != 4);
+    while(proby != 3);
 
-    if(flaga) return true;
-    else System.Console.WriteLine("Jesteś nie trzeźwy!");
-    return false;
-} // BRAKUJE OBLICZENIA RZECZYWISTEGO WYNIKU
-System.Console.WriteLine(trzezwosc());
+    if(!flaga)Console.WriteLine("Jesteś nie trzeźwy!");
+
+    return flaga;
+}
+
+static void kalkulator()
+{
+    bool repeat = true;
+    do{
+        Console.WriteLine("Podaj działanie które chcesz wykonać:");
+        string dzialanie = Console.ReadLine();
+        
+        DataTable dt = new();
+        var wyn = dt.Compute(dzialanie, "");
+        decimal wynik = Convert.ToDecimal(wyn);
+        Console.WriteLine("Wynik: " + wynik);
+
+        Console.WriteLine("Czy chcesz wykonać kolejne działanie? T - Tak, N - Nie");
+        string powtorz = Console.ReadLine();
+        if(powtorz != "T" && powtorz != "t") repeat = false;
+
+    }while(repeat);
+
+    Console.WriteLine("Dowidzenia!");
+}
+
