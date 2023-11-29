d563d774a1b1dc6e7a10eadbaa6c96635ef437bd
The Fisher <114557710+RadomirSz@users.noreply.github.com>
2023-11-29 15:14:05 +0100

    rekurencja ;-;

--- Program.cs
+++ Program.cs
@@ -1,27 +1,108 @@
-﻿
-/*string l = Console.ReadLine();
-int s = int.Parse(Console.ReadLine());
-int liczba = (int)l[0]-48;
-for (int i = 1; i < l.Length; i++)
+﻿/*for (int i = 0; i < 6; i++)
 {
-    liczba = liczba *s + (l[i] - 48);
+    Console.WriteLine(i);
 }
-Console.WriteLine(liczba);*/
 
-//bin -> dec
-/*string b = Console.ReadLine();
-int liczba = (int)b[0]-48;
-for (int i = 1; i < b.Length; i++) liczba = liczba * 2 + (b[i] - 48);
-Console.WriteLine(liczba);
+Console.WriteLine();*/
+
+/*void rel(int n)
+{
+    if (n == 6) return;
+    Console.WriteLine(n + " ");
+    rel(n + 1);
+}
+
+rel(1);*/
+
+/*void re2(int n)
+{
+    if (n==0) return;
+    Console.Write(n + " ");
+    re2(n - 1);
+    Console.Write(n + " ");
+}
+re2(6);*/
+
+
+/*int suma = 0;
+for(int i = 0; i < 10; i++) 
+{
+    suma += i;
+}
+Console.WriteLine(suma);
+int re3(int n)
+{
+    if(n == 0) return 0;
+    return re3(n - 1) + n;
+}
+Console.WriteLine(re3(9));*/
+
+
+//####################################
+
+/*int s4 = 0;
+for (int i = 10; i < 100; i += 2)
+{
+    s4 += i;
+}
+Console.WriteLine(s4);
+
+int re4(int n)
+{
+    if (n < 10) return 0;
+    return re4(n - 2) + n;
+}
+Console.WriteLine(re4(98));*/
+
+//#######################################
+int n = 4;
+/*int re5(int n)
+{
+    if (n == 1) return 3;
+    return re5(n - 1) + n * 3;
+}
+Console.WriteLine(re5(3));
+
+int re5b(int n)
+{ 
+    if(n==1) return 12;
+    return re5b(n - 1) + 11;
+}
+
+int s5b = 0;
+for (int i = 0; i <= n; i++)
+{
+    s5b += re5b(i);
+}*/
+
+// 1,4,9,16
+/*int r5d(int n)
+{
+    if(n == 1) return 1;
+    return r5d(n - 1) + 2 * (n - 1) + 1;
+}
+int s5d = 0;
+for (int i = 0; i <= n; i++)
+{
+    s5d += r5d(i);
+}
+
+Console.WriteLine(s5d);
+
 */
-//dec -> bin
 
-int d = int.Parse(Console.ReadLine());
-string bin = "";
-while (d > 0)
+
+double r6a(int n)
 {
-    if (d % 2 == 1) bin= "1" + bin;
-    else bin = "0" + bin;
-    d /= 2;
+    if (n == 1) return 1.5;
+    if (n == 2) return 1;
+    if (n == 3) return 0.5;
+    return r6a(n - 1) * 2 - r6a(n - 3);
 }
-Console.WriteLine(bin);
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(r6a(i) + " ");
+}
+
+
